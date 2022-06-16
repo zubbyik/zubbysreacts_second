c4f159b15cbf236d3e858a04f44f8507421e5a36
Malachy E <zubbyik@gmail.com>
2022-06-16 13:29:44 +0200

    fourth commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 
 const Greeting = ()=>{
 
-    return <h4>Hello I am the new h5</h4>
+    return (<h4>Hello I am the new h5</h4>)
 }
 
 ReactDOM.render(<Greeting/>, document.getElementById("root"))
