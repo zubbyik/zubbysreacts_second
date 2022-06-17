b7fa20644f20297c9f795ff3e67b6fe22095e1eb
Malachy E <zubbyik@gmail.com>
2022-06-17 17:57:05 +0200

    seventh commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react"
 
-const Book =({children})=> <h4>Hello from the book component => {children} </h4>
-
+const Book =({name, age, startSign})=>{
+    return (
+    <section>
+        <h1>Title</h1>
+        <h4>Hello my name is {name}</h4>
+        <h4> I am {age} years old</h4>
+        <h5>My star sign in {startSign}</h5>
+    </section>
+    )
+        }
+    
 export default Book
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Book from "./App.jsx"
+import Mydata from  "./data"
 
 const Greeting = ()=>{
-    return (<Book>This is the inner child component
-        </Book>)
+    return (
+        Mydata.map((data)=>{
+            return   <Book {...data}></Book>
         }
+        )
+    )
+}
 
 ReactDOM.render(<Greeting/>, document.getElementById("root"))
